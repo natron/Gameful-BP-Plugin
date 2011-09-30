bad0b28f7e6e04dfde4562ff0060531f983068b5
Mark Borcherding <markborcherding@gmail.com>
2011-09-29 19:48:41 -0500

    closing textarea

--- includes/bp-gameful-donate.php
+++ includes/bp-gameful-donate.php
@@ -4,7 +4,7 @@
 		?>
 		<div class="field donate-why" > 
 			<h2>Why?</h2>
-			<textarea name="donateWhy" id="donateWhy" />
+			<textarea name="donateWhy" id="donateWhy" ></textarea>
 		</div>
 		<?php
 	}
